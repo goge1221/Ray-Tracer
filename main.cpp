0944e8f143a0d6967284b0025dcea9d61d7dc613
Goje Andrei <a12032793@univie.ac.at>
2023-05-08 18:38:28 +0200

    Outputs a normal standard image with a verlauf.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
+#include "color.h"
+#include "vec3.h"
+#include "ray.h"
+
+color ray_color(const ray& r){
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    // Image
+    const int image_width = 256;
+    const int image_height = 256;
+
+    // Render
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height-1; j >= 0; --j) {
+        // Used for keeping track of the progress, can be deleted after
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+
+        for (int i = 0; i < image_width; ++i) {
+            color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
+            write_color(std::cout, pixel_color);
+        }
+    }
+    std::cerr << "\nDone\n";
 }
