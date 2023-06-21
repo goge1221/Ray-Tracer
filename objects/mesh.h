cc3d777ba086a1e4e1658765e4a9a2a720170298
Goje Andrei <a12032793@univie.ac.at>
2023-06-21 08:29:54 +0200

    Parsing the square is working

--- objects/mesh.h
+++ objects/mesh.h
@@ -25,12 +25,9 @@ public:
     void parse_vertices(const char *filePath) {
         OBJParser::loadOBJ(filePath, vertices);
 
-        //behindWall behindWall(-5, 5, -2.5, 7.5, -10);
-        //bottomWall bwall(-5, 5, -10, 0, -2.5);
-
         bwall = behindWall(-5, 5, -2.5, 7.5, -10);
-        rwall = rightWall(-2.5, 7.5, -10, 0, 5);;
-        boWall = bottomWall(-5, 5, -10, 0, -2.5);
+
+        initialize_walls();
     }
 
     bool square_hit(const ray &ray, hit_information &hit_info) {
@@ -44,6 +41,61 @@ public:
     }
 
     Mesh() = default;
+
+private:
+
+    void initialize_walls() {
+        for (int i = 0; i < 3; ++i) {
+            vec3 firstVertex = vertices[0 + i * 4];
+            vec3 secondVertex = vertices[1 + i * 4];
+            vec3 thirdVertex = vertices[2 + i * 4];
+            vec3 fourthVertex = vertices[3 + i * 4];
+
+            if (are_numbers_the_same(firstVertex.x(), secondVertex.x(), thirdVertex.x(), fourthVertex.x())) {
+                //find min y and min z and add x as a position
+                //this is the right wall
+                double y_min = get_minimum(firstVertex.y(), secondVertex.y(), thirdVertex.y(), fourthVertex.y());
+                double y_max = get_maximum(firstVertex.y(), secondVertex.y(), thirdVertex.y(), fourthVertex.y());
+                double z_min = get_minimum(firstVertex.z(), secondVertex.z(), thirdVertex.z(), fourthVertex.z());
+                double z_max = get_maximum(firstVertex.z(), secondVertex.z(), thirdVertex.z(), fourthVertex.z());
+                double position_on_x_axis = firstVertex.x();
+                rwall = rightWall(y_min, y_max, z_min, z_max, position_on_x_axis);;
+
+            } else if (are_numbers_the_same(firstVertex.y(), secondVertex.y(), thirdVertex.y(), fourthVertex.y())) {
+                //this is the bottom wall, y is the position and x and z are the positions
+                double x_min = get_minimum(firstVertex.x(), secondVertex.x(), thirdVertex.x(), fourthVertex.x());
+                double x_max = get_maximum(firstVertex.x(), secondVertex.x(), thirdVertex.x(), fourthVertex.x());
+                double z_min = get_minimum(firstVertex.z(), secondVertex.z(), thirdVertex.z(), fourthVertex.z());
+                double z_max = get_maximum(firstVertex.z(), secondVertex.z(), thirdVertex.z(), fourthVertex.z());
+                double position_on_x_axis = firstVertex.y();
+                boWall = bottomWall(x_min, x_max, z_min, z_max, position_on_x_axis);;
+
+            } else {
+                //this is the behind wall
+                //z is the position and x and y are the things need to be parsed
+                double x_min = get_minimum(firstVertex.x(), secondVertex.x(), thirdVertex.x(), fourthVertex.x());
+                double x_max = get_maximum(firstVertex.x(), secondVertex.x(), thirdVertex.x(), fourthVertex.x());
+                double y_min = get_minimum(firstVertex.y(), secondVertex.y(), thirdVertex.y(), fourthVertex.y());
+                double y_max = get_maximum(firstVertex.y(), secondVertex.y(), thirdVertex.y(), fourthVertex.y());
+                double position_on_x_axis = firstVertex.z();
+                bwall = behindWall(x_min, x_max, y_min, y_max, position_on_x_axis);;
+
+            }
+        }
+    }
+
+    double get_maximum(double first, double second, double third, double fourth) {
+        return std::max({first, second, third, fourth});
+    }
+
+    double get_minimum(double first, double second, double third, double fourth) {
+        return std::min({first, second, third, fourth});
+    }
+
+    bool are_numbers_the_same(double first, double second, double third, double fourth) {
+        return (first == second) && (second == third) && (third == fourth);
+    }
+
 };
 
 #endif //LAB_3_MESH_H
